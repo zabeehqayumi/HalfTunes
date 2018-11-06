eada7bcffda481956d2fb3293b39b7b3691b7c63
Zabeehullah Qayumi <32587905+zabeehqayumi@users.noreply.github.com>
2018-11-05 21:44:32 -0800

    Setup method sat up :)

--- HalfTunesTests/HalfTunesTests.swift
+++ HalfTunesTests/HalfTunesTests.swift
@@ -8,19 +8,31 @@
 
 import XCTest
 
+@testable import HalfTunes
+
+
+
 class HalfTunesTests: XCTestCase {
+    
+    var sessionUnderTest : URLSession!
+
+    
 
     override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        
+        super.setUp()
+        
+        sessionUnderTest = URLSession(configuration: URLSessionConfiguration.default)
+        
+   
     }
 
     override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+  
     }
 
     func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+ 
     }
 
     func testPerformanceExample() {
